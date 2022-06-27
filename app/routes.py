711da0b4902afe11622c050bbb5e9905f2bac2e8
suph4rd <suph4rd575@gmail.com>
2022-06-28 00:21:40 +0300

    fix: add interactive load of information from bank

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,4 @@ from . import views
 
 def setup_routes(app: web.Application) -> None:
     app.router.add_view('/', views.MainPage)
+    app.router.add_view('/belarusbank-view/', views.BelarusbankBankTemplateView)
