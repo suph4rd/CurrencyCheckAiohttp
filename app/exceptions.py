8ddcc3b27bb0fc98329776373cbc46acc47024cc
suph4rd <suph4rd575@gmail.com>
2022-07-02 20:28:11 +0300

    feat: add DatabaseSaveError

--- app/exceptions.py
+++ app/exceptions.py
@@ -5,3 +5,10 @@ class HandleError(Exception):
     def __str__(self):
         print(super().__str__())
         return "Ошибка распаковки ответа!"
+
+
+class DatabaseSaveError(Exception):
+
+    def __str__(self):
+        print(super().__str__())
+        return "Ошибка записи объекта в базу данных!"
